04405450d34b0e2dde3b42c76da092394e378e3f
Ihor <onishchenko.ihor@gmail.com>
2021-04-12 21:17:47 +0300

    fix 02_lesson task-2

--- 02_Operators&&Conditions/task_2/script.js
+++ 02_Operators&&Conditions/task_2/script.js
@@ -3,9 +3,11 @@
 let num = +prompt('Задайте число чтобы проверить его делимость на 3 и 5')
 
 if (num % 3 === 0) {
-    alert('Fitz');
+    if (num % 5 === 0) {
+        alert('Fitz Butz');
+    } else {
+        alert('Fitz');
+    }
 } else if (num % 5 === 0) {
     alert('Butz');
-} else if ((num % 3 === 0) && (num % 5 === 0)) {
-    alert('Fitz Butz');
 };
