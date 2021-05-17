94e30fac6599f1d36d310eae00e44a5dbcf1818e
Peter Ilyash <p.ilyash.j@gmail.com>
2021-05-17 21:51:43 +0300

    Add 'push' method of 'list-object' task

--- 05_Objects/task_02/script.js
+++ 05_Objects/task_02/script.js
@@ -2,17 +2,36 @@ const list = {
     1: 'Breaking Bad',
     2: 'Game of Thrones',
     3: 'Lost',
+    length: 3,
 
     push() {
         this[4] = 'Eight Sense';
     },
+
     pop() {
         delete this[4];
-     },
-    shift() {
-        delete this[1];
     },
+
+    shift() {
+        const shiftElem = this[1];
+
+        for (let i = 1; i < this.length; i++) {
+            this[i] = this[i + 1];
+        }
+
+        delete this[this.length];
+        this.length--;
+
+        return shiftElem;
+    },
+
     unshift() {
         this[1] = 'Babylon 5'
-     },
+    },
 };
+
+console.log(list);
+
+list.shift();
+console.log(list);
+
