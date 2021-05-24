37f84854db4fbf7a5e0d002fc0e77ffbfda1f80b
Ihor <onishchenko.ihor@gmail.com>
2021-05-24 21:18:37 +0300

    fix 05_object task 2

--- 05_Objects/task_02/script.js
+++ 05_Objects/task_02/script.js
@@ -4,34 +4,64 @@ const list = {
     3: 'Lost',
     length: 3,
 
-    push() {
-        this[4] = 'Eight Sense';
+    push(item) {
+        this[4] = item;
+        this.length++;
+        // for (let key in list) {
+        //     if (this.length) {
+
+        //     }
+        // }
     },
 
     pop() {
-        delete this[4];
-    },
+        const popElem = this[1];
 
-    shift() {
-        const shiftElem = this[1];
-
-        for (let i = 1; i < this.length; i++) {
+        for (let i = 4; i < this.length; i++) {
             this[i] = this[i + 1];
         }
 
         delete this[this.length];
         this.length--;
 
+        return popElem;
+    },
+
+    shift() {
+        const shiftElem = this[1];
+        let length = this.length;
+
+        for (let i = 1; i < length; i++) {
+            this[i] = this[i + 1];
+        }
+
+        delete this[length];
+        this.length--;
+
         return shiftElem;
     },
 
-    unshift() {
-        this[1] = 'Babylon 5'
+    unshift(item) {
+        this[1] = item;
+        this.length++;
     },
 };
 
-console.log(list);
+// console.log(list);
 
-list.shift();
-console.log(list);
+list.push('Eight Sense');
+console.log(list)
+
+list.pop();
+console.log(list)
+
+// list.shift();
+// console.log(list);
+
+// list.unshift('Babylon 5');
+// console.log(list);
+
+
+
+
 
