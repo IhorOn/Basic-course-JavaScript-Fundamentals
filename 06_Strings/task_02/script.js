1236cfdd9958ce63642f1fe9da117e0ac3af39c9
Ihor <onishchenko.ihor@gmail.com>
2021-05-25 18:54:26 +0300

    add 06_strings task_02

---
+++ 06_Strings/task_02/script.js
@@ -0,0 +1,17 @@
+let lineComment = '// A comment';
+let multilineComment = '/* A comment */';
+let notComment = 'Not a comment';
+
+function checkIsComment(str) {
+    if (str.indexOf('/*' && '*/') > 0) {
+        return true;
+    } else if (str.indexOf('//') >= 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(checkIsComment(multilineComment));
+console.log(checkIsComment(lineComment));
+console.log(checkIsComment(notComment));
