b0ad3c82f36d21a7c9ac131f94f53bd75876eb06
Ihor <onishchenko.ihor@gmail.com>
2021-06-18 11:30:29 +0300

    fix 08_arrays/02_task

--- 08_Arrays/02_task/script.js
+++ 08_Arrays/02_task/script.js
@@ -1,13 +1,19 @@
 const grid = [[0, 1, 2], [3, 4, 5]];
 deepMap(grid, item => 10 * item);
 
+
 function deepMap(newGrid, func) {
+	let arrItem = []
 	newGrid = grid.map(item => {
+		let arrNum = []
+
+		arrItem.push(arrNum)
+		
 		item.map(num => {
-			func(num)
-			console.log(func(num));
+			arrNum.push(func(num))
 		})
 	})
+	console.log(arrItem)
 }
 
 
