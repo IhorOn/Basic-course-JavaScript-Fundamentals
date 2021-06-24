d799b1bfcd03926eb45688a08e8f755e2576033e
Ihor <onishchenko.ihor@gmail.com>
2021-06-24 12:19:43 +0300

    add 08_Arrays/03_task

---
+++ 08_Arrays/03_task/script.js
@@ -0,0 +1,28 @@
+
+let set = [
+    [2, 32, 1],
+    [12, 45, 5],
+    [56, 81, 7]
+];
+
+let setCon = [].concat(set[0], set[1], set[2]).sort((a, b) => a - b);
+let setSort = [[], [], []]
+
+setCon.forEach((number, index) => {
+    if (index <= 2) {
+        setSort[0].push(number)
+    } else if (index <= 5) {
+        setSort[1].push(number)
+    } else if (index <= 8) {
+        setSort[2].push(number)
+    }
+});
+
+console.log(setSort)
+
+
+
+
+
+
+
