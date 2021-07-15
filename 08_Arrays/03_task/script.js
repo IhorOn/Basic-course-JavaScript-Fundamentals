38299f05e03e4de85e6049f1df5d7bda05187b53
Ihor <onishchenko.ihor@gmail.com>
2021-07-15 12:11:01 +0300

    final fixed 08_Arrays/03_task

--- 08_Arrays/03_task/script.js
+++ 08_Arrays/03_task/script.js
@@ -1,27 +1,79 @@
 let set = [
-    [2, 32, 1],
-    [12, 45, 5],
-    [56, 81, 7]
+	['H', 'H', 'W', 'O'],
+	['W', 'W', 'O', 'W'],
+	['H', 'H', 'O', 'O'],
 ];
 
-let setWidth = set[0].length;
-let setHeight = set.length;
+let liquids = {
+	'H': 1.36,
+	'W': 1,
+	'A': 0.87,
+	'O': 0.8,
+};
 
-let setCon = [].concat(set[0], set[1], set[2]).sort((a, b) => a - b); //second
+let liquidsRevers = {};
+let sortingArr = [];
+let arrConcat = [];
+let arrWidth = set[0].length;
+let arrHeight = set.length;
 
-let setSort = [[], [], []]; // first
+tableRevers(liquids);
 
-setCon.forEach((number, index) => {
-    if (index <= 2) { //third
-        setSort[0].push(number)
-    } else if (index <= 5) {
-        setSort[1].push(number)
-    } else if (index <= 8) {
-        setSort[2].push(number)
-    }
-});
+transformTypeElem(set, liquids);
 
-console.log(setSort);
+copyOriginalArray(set);
+
+concatToOneArray(set);
+
+sortArray(arrConcat);
+
+recordsSourceArray(arrConcat);
+
+transformTypeElem(sortingArr, liquidsRevers);
+
+function tableRevers(liquids) {
+	for (let key in liquids) {
+		liquidsRevers[liquids[key]] = key;
+	};
+};
+
+function transformTypeElem(array, table) {
+	for (let i = 0; i < arrHeight; i++) {
+		let newArr = array[i];
+		for (let j = 0; j < arrWidth; j++) {
+			newArr[j] = table[newArr[j]];
+		};
+	};
+};
+
+function copyOriginalArray() {
+	for (let i = 0; i < arrHeight; i++) {
+		let newArr = [];
+		sortingArr.push(newArr);
+	}
+};
+
+function concatToOneArray(array) {
+	for (let i = 0; i < arrHeight; i++) {
+		arrConcat = arrConcat.concat(array[i]);
+	};
+};
+
+function sortArray(array) {
+	array.sort((a, b) => a - b);
+};
+
+function recordsSourceArray(array) {
+	for (let i = 0; i < array.length; i++) {
+		for (let j = 0; j < arrHeight; j++) {
+			if (j * arrWidth <= i && i < (j + 1) * arrWidth) {
+				sortingArr[j].push(arrConcat[i]);
+			};
+		};
+	};
+};
+
+console.log(sortingArr);
 
 
 
