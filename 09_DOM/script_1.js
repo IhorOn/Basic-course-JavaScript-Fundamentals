f3a064df53d4126712db789b41be818886bf0609
Ihor <onishchenko.ihor@gmail.com>
2021-07-15 17:01:58 +0300

    add 09_DOM/page_1

---
+++ 09_DOM/script_1.js
@@ -0,0 +1,21 @@
+const documentBody = document.body;
+const arrayElem = [];
+
+const navElem = documentBody.firstElementChild; //1
+arrayElem.push(navElem);
+
+const nextSiblingNav = navElem.nextElementSibling; //2
+arrayElem.push(nextSiblingNav);
+
+arrayElem.push(navElem.firstElementChild.children[2]); //3
+
+arrayElem.push(nextSiblingNav.parentElement); //4
+
+const tablePricing = documentBody.querySelectorAll('td'); //5
+for (let cell of tablePricing) {
+    arrayElem.push(cell)
+};
+
+console.log(arrayElem);
+
+
