b70ef3d8bc9db75fdb547f427aaf50094edb3682
Ihor <onishchenko.ihor@gmail.com>
2021-08-27 22:01:36 +0300

    test task01

--- 10_Document_2/01_task/script.js
+++ 10_Document_2/01_task/script.js
@@ -6,4 +6,11 @@ console.log(getRandom());
 function getRandomArbitrary(min, max) {
   return Math.random() * (max - min) + min;
 }
-console.log(getRandomArbitrary(0, 500))
+console.log(getRandomArbitrary(0, 500));
+
+let circle = document.querySelector('.content__circle');
+
+let content = document.querySelector('.content');
+
+circle.style.left = '400px'
+circle.style.top = '220px'
