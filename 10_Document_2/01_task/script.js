f035a2520a07188c3c135429ef32c7a51208ce16
Ihor <onishchenko.ihor@gmail.com>
2021-08-27 22:09:06 +0300

    text task02

--- 10_Document_2/01_task/script.js
+++ 10_Document_2/01_task/script.js
@@ -1,16 +1,12 @@
-function getRandom() {
-  return Math.random();
-}
-console.log(getRandom());
-
 function getRandomArbitrary(min, max) {
   return Math.random() * (max - min) + min;
 }
-console.log(getRandomArbitrary(0, 500));
+let random = getRandomArbitrary(0, 500);
 
 let circle = document.querySelector('.content__circle');
 
 let content = document.querySelector('.content');
 
+circle.style.top = random + 'px';
 circle.style.left = '400px'
-circle.style.top = '220px'
+
