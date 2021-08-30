7647482e1ae33c8cda45d7793c74c992aba7c99f
Ihor <onishchenko.ihor@gmail.com>
2021-08-30 21:43:09 +0300

    fix 10_document_2/01_task

--- 10_Document_2/01_task/script.js
+++ 10_Document_2/01_task/script.js
@@ -1,12 +1,26 @@
 function getRandomArbitrary(min, max) {
   return Math.random() * (max - min) + min;
-}
-let random = getRandomArbitrary(0, 500);
+};
 
+let random = getRandomArbitrary(0, 400);
 let circle = document.querySelector('.content__circle');
+let counter = document.querySelector('.circle__counter');
+let position = 0;
+let clearInt = setInterval(moveCircle, 5);
 
-let content = document.querySelector('.content');
+function moveCircle() {
+    if (position == 400) {
+        clearInterval(clearInt);
+        circle.style.background = 'blue';
+        counter.innerHTML = 2;
+    } else {
+        position++;
+        circle.style.top = position + 'px';
+        circle.style.left = position + 'px';
+    }
+}
 
-circle.style.top = random + 'px';
-circle.style.left = '400px'
+// circle.style.top = random + 'px';
+// circle.style.left = '400px'
+
 
