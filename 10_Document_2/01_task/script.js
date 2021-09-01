2e8d52bfcc7b5847da772da2a4993411c7a46b37
Ihor <onishchenko.ihor@gmail.com>
2021-09-01 21:20:24 +0300

    fix 10_document_2/01_task

--- 10_Document_2/01_task/script.js
+++ 10_Document_2/01_task/script.js
@@ -5,21 +5,30 @@ function getRandomArbitrary(min, max) {
 let random = getRandomArbitrary(0, 400);
 let circle = document.querySelector('.content__circle');
 let counter = document.querySelector('.circle__counter');
-let position = 0;
-let clearInt = setInterval(moveCircle, 5, 390,112);
+let time = 1000;
+let clearInt = setInterval(moveCircle, 5, 300, 100);
+let currentX = 0;
+let currentY = 0;
+let n = time / 5;
 
-function moveCircle(x,y) {
-    if (x == 400 || y == 400) {
+function moveCircle(x, y) {
+    let stepX = x / n;
+    let stepY = y / n;
+
+    currentX += stepX;
+    currentY += stepY;
+    
+    if (currentY >= 400 || currentX >= 400) {
         clearInterval(clearInt);
         circle.style.background = 'blue';
         counter.innerHTML = 2;
     } else {
-        x += 2;
-        y += 2;
-        circle.style.top = x + 'px';
-        circle.style.left = y + 'px';
+        currentX++;
+        currentY++;
+        circle.style.top = currentY + 'px';
+        circle.style.left = currentX + 'px';
     }
-}
+};
 
 // circle.style.top = random + 'px';
 // circle.style.left = '400px'
--- test/script.js
+++ test/script.js
@@ -1,3 +1,28 @@
-let nameJS = prompt("Какое официальное название JavaScript?", '');
+// let clearInt = setInterval(moveCircle, 5, 390,112);
 
-let result = (nameJS == 'ECMAScript') ? console.log('Верно!') : console.log('Неверно!');
+function moveCircle(x, y) {
+    let currentX = 0;
+    let currentY = 0;
+    let stepX = x / n;
+    let stepY = y / n;
+    let n = time / 5;
+    
+    setInterval(() => {
+        currentX += stepX;
+        currentY += stepY;
+    }, 5);
+
+    if (x == 400 || y == 400) {
+        clearInterval(clearInt);
+        circle.style.background = 'blue';
+        counter.innerHTML = 2;
+    } else {
+        stepX++
+        stepY++
+        circle.style.top = stepY + 'px';
+        circle.style.left = stepX + 'px';
+    }
+}
+
+// circle.style.top = random + 'px';
+// circle.style.left = '400px'
